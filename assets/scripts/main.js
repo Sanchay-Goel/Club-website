2e12014455b9cc61289e623f53a01e8e75ebfb7c
Sanchay-Goel <sanchay.17je003408@cse.iitism.ac.in>
2020-03-12 13:11:53 +0530

    Navbar and carousal added

---
+++ assets/scripts/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    $('.menu-toggle').on('click', function(){
+        console.log('menu clicked !!');
+        $('.nav').toggleClass('showing');
+    });
+
+    $('.post-wrapper').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        nextArrow: $('.next'),
+        prevArrow: $('.prev'),
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+              }
+            },
+            {
+              breakpoint: 620,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+      });
+
+});
